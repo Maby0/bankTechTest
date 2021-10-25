b109f84d47b67eb9ebb21576ee99caf3eb135c94
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-10-25 20:06:20 +0100

    rgr for getting current balance

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -9,4 +9,11 @@ describe("Account", function () {
       expect(account.getTransactionLog().length).toEqual(1);
     })
   })
+
+  describe("getCurrentBalance", function () {
+    it("returns the correct balance value", function () {
+      account.newTransaction("deposit", 1);
+      expect(account.getBalance()).toEqual("1.00");
+    })
+  })
 })
--- src/account.js
+++ src/account.js
@@ -13,7 +13,20 @@ class Account {
     return this.transactionLog;
   }
 
+  getBalance() {
+    this._updateBalance();
+    return this.balance.toFixed(2);
+  }
+
   _newTransactionInstance(type, value) {
     return new Transaction(this.balance, type, value);
+  }
+
+  _updateBalance() {
+    this.balance = this._getMostRecentBalance()
+  }
+
+  _getMostRecentBalance() {
+    return this.transactionLog[this.transactionLog.length - 1].updatedBalance;
   }
 }
